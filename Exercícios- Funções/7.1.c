79fe3d34a0c2dd8f5734848eae39964aafd1d6aa
= <=>
2023-05-16 15:08:52 -0300

    update ex7.1

--- Exercícios- Funções/7.1.c
+++ Exercícios- Funções/7.1.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-
+int absoluto(int n);
 
 int main(){
+    int array[5];
+
+    for(int i = 0; i < 5; i++){
+        printf("n%d: ", i);
+        scanf("%d", &array[i]);
+        array[i] = absoluto(array[i]);
+    }
+
+    for(int i = 0; i < 5; i++){
+        
+    }
+
 
     return 0;
+}
+
+int absoluto(int n){
+    return n < 0 ? -n : n;
 }
