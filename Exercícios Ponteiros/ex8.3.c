ee3eb1a0d35b5a532af234e67a0ede006c4719e1
JaoMachado <jp.dausi@hotmail.com>
2023-05-25 16:57:48 -0300

    ex8.2 e 8.3 finalizados

--- Exercícios Ponteiros/ex8.3.c
+++ Exercícios Ponteiros/ex8.3.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void decompoeTempo( int totalSeg, int *horas, int *minutos, int *seg );
+
 int main(){
-    
+    int totalSeg;
+    int horas;
+    int minutos;
+    int seg;
+
+    printf("Total de segundos: ");
+    scanf("%d", &totalSeg);
+
+    decompoeTempo(totalSeg, &horas, &minutos, &seg);
+
+    printf("%d  segundo(s) corresponde(m) a:\n    %d hora(s)\n    %d minuto(s)\n    %d segundo(s)", totalSeg, horas, minutos, seg);
+}
+
+void decompoeTempo( int totalSeg, int *horas, int *minutos, int *seg ){
+    *horas = totalSeg / 3600;
+    *minutos = (totalSeg % 3600) / 60;
+    *seg = (totalSeg % 3600) % 60;
 }
