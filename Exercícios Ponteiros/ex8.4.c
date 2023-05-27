b15a36d994e322286b7ffb2c0dd259fd46b5d120
JaoMachado <133260624+JaoMachado@users.noreply.github.com>
2023-05-26 23:33:43 -0300

    Add files via upload

---
+++ Exercícios Ponteiros/ex8.4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void decompoeData( int diaDoAno, int ano, int *mes, int *dia );
+bool ehBissexto( int ano );
+
+int main(){
+    int diaDoAno;
+    int ano;
+    int mes;
+    int dia;
+
+    printf("Dia do ano: ");
+    scanf("%d", &diaDoAno);
+
+    printf("Ano: ");
+    scanf("%d", &ano);
+
+    decompoeData(diaDoAno, ano, &mes, &dia);
+
+    printf("O dia %d do ano %d cai no dia %d do mes %d.", diaDoAno, ano, dia, mes);
+}
+
+void decompoeData( int diaDoAno, int ano, int *mes, int *dia ){
+    if(diaDoAno < 32){
+        *mes = diaDoAno / 30;
+    } else {
+        *mes = diaDoAno / 30 + 1;
+    }
+    
+    if(ehBissexto(ano)){
+        if(*mes == 2 || *mes == 1){
+            *dia = diaDoAno - (*mes - 1) * 31;
+        } else {
+            *dia = (diaDoAno - (*mes - 1) * 30) - 1;
+        }
+    } else {
+        *dia = diaDoAno - (*mes - 1) * 30;
+    }
+}
+
+bool ehBissexto( int ano ){
+    if(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0){
+        return true;
+    }
+    return false;
+}
