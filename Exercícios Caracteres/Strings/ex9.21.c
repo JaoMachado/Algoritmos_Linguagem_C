205e28c5e9d6114fab1968d97522bf23877a2a9b
= <=>
2023-06-10 13:43:25 -0300

    Lista 9 finalizada

--- Exercícios Caracteres/Strings/ex9.21.c
+++ Exercícios Caracteres/Strings/ex9.21.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void imprimirCentralizado( const char *str );
 
 int main(){
+    char string[40];
 
+    printf("String: ");
+    fgets(string, 40, stdin);
+    string[strlen(string)-1] = '\0';
+
+    imprimirCentralizado(string);
     return 0;
+}
+
+void imprimirCentralizado( const char *str ){
+    for(int i = 0; i < (40-strlen(str)); i++){
+        printf(" ");
+    }
+    
+    printf("%s", str);
 }
