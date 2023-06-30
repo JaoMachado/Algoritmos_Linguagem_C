b073f1504e1e50bd752f029cab5770098a1d46ee
= <=>
2023-06-30 00:35:51 -0300

    Até ex10.13 feito

--- Exercícios Estruturas/ex10.12.c
+++ Exercícios Estruturas/ex10.12.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int x;
+    int y;
+} Ponto;
+
+typedef struct{
+    Ponto superiorEsquerdo;
+    Ponto inferiorDireito;
+} Retangulo;
+
+Retangulo novoRetangulo( const Ponto *sEsq, const Ponto *iDir );
+Ponto obterCentro( const Retangulo *r );
+void imprimirRetangulo( const Retangulo *r );
+
 int main() {
+    Ponto p1;
+    Ponto p2;
+    Retangulo r;
+
+    printf("Ponto superior esquerdo\n");
+    printf("x: ");
+    scanf("%d", &p1.x);
+    printf("y: ");
+    scanf("%d", &p1.y);
+    
+    printf("\nPonto superior esquerdo\n");
+    printf("x: ");
+    scanf("%d", &p2.x);
+    printf("y: ");
+    scanf("%d", &p2.y);
+
+    r = novoRetangulo(&p1, &p2);
+    imprimirRetangulo(&r);
+
+    printf("Centro: (%+02d, %+02d)", obterCentro(&r).x, obterCentro(&r).y);
 
     return 0;
+}
+
+Retangulo novoRetangulo( const Ponto *sEsq, const Ponto *iDir ){
+    Retangulo r;
+    r.superiorEsquerdo = *sEsq;
+    r.inferiorDireito = *iDir;
+    return r;
+}
+
+Ponto obterCentro( const Retangulo *r ){
+    Ponto p;
+    p.x = (r->superiorEsquerdo.x + r->inferiorDireito.x) / 2;
+    p.y = (r->superiorEsquerdo.y + r->inferiorDireito.y) / 2;
+    return p;
+}
+
+void imprimirRetangulo( const Retangulo *r ){
+    printf("(%+02d, %+02d) =====|\n", r->superiorEsquerdo.x, r->superiorEsquerdo.y);
+    printf("|               |\n");
+    printf("|               |\n");
+    printf("|===== (%+02d, %+02d)\n", r->inferiorDireito.x, r->inferiorDireito.y);
 }
--- Exercícios Estruturas/ex10.13.c
+++ Exercícios Estruturas/ex10.13.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int x;
+    int y;
+} Ponto;
+
+typedef struct{
+    Ponto superiorEsquerdo;
+    Ponto inferiorDireito;
+} Retangulo;
+
+Retangulo novoRetangulo( const Ponto *sEsq, const Ponto *iDir );
+void mover( Retangulo *r, int x, int y );
+void imprimirRetangulo( const Retangulo *r );
+
 int main() {
+    Ponto p1;
+    Ponto p2;
+    Retangulo r;
+    int x;
+    int y;
+
+    printf("Ponto superior esquerdo\n");
+    printf("x: ");
+    scanf("%d", &p1.x);
+    printf("y: ");
+    scanf("%d", &p1.y);
+    
+    printf("\nPonto superior esquerdo\n");
+    printf("x: ");
+    scanf("%d", &p2.x);
+    printf("y: ");
+    scanf("%d", &p2.y);
+
+    printf("Mover em x: ");
+    scanf("%d", &x);
+
+    printf("Mover em y: \n");
+    scanf("%d", &y);
+
+    r = novoRetangulo(&p1, &p2);
+
+    printf("Retangulo original:\n");
+    imprimirRetangulo(&r);
+    
+    mover(&r, x, y);
+
+    printf("Retangulo movido:\n");
+    imprimirRetangulo(&r);
 
     return 0;
+}
+
+Retangulo novoRetangulo( const Ponto *sEsq, const Ponto *iDir ){
+    Retangulo r;
+    r.superiorEsquerdo = *sEsq;
+    r.inferiorDireito = *iDir;
+    return r;
+}
+
+void mover( Retangulo *r, int x, int y ){
+    r->superiorEsquerdo.x += x;
+    r->superiorEsquerdo.y += y;
+    r->inferiorDireito.x += x;
+    r->inferiorDireito.y += y;
+}
+
+void imprimirRetangulo( const Retangulo *r ){
+    printf("(%+02d, %+02d) =====|\n", r->superiorEsquerdo.x, r->superiorEsquerdo.y);
+    printf("|               |\n");
+    printf("|               |\n");
+    printf("|===== (%+02d, %+02d)\n", r->inferiorDireito.x, r->inferiorDireito.y);
 }
